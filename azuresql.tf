ed62555fd3880262b902e6bc200b88460d298db9
Francisco Rodriguez <168039749+franciscolrm86@users.noreply.github.com>
2024-05-17 19:11:06 -0400

    Update azuresql.tf

--- azuresql.tf
+++ azuresql.tf
@@ -1,23 +1,55 @@
 resource "azurerm_resource_group" "francisco_sql" {
-  name     = "database-rg"
-  location = "Canada Central"
+  name     = var.name
+  location = var.location
 }
 
 resource "azurerm_mssql_server" "francisco_sql" {
-  name                         = "mysqlserver"
+  name                         = var.mysqlname
   resource_group_name          = azurerm_resource_group.francisco_sql.name
   location                     = azurerm_resource_group.francisco_sql.location
-  version                      = "12.0"
-  administrator_login          = "francisco_admin"
-  administrator_login_password = "Monica,127"
-  minimum_tls_version          = "1.2"
+  version                      = var.resourceversion
+  administrator_login          = var.administrator_login
+  administrator_login_password = var.administrator_login_password
+  minimum_tls_version          = var.minimum_tls_version
 
   azuread_administrator {
-    login_username = "AzureAD Admin"
-    object_id      = "00000000-0000-0000-0000-000000000000"
+    login_username = var.login_username
+    object_id      = var.object_id
   }
 
   tags = {
     environment = "production"
+  }
+}
+
+# Below created via yaml
+locals{
+  sqlserverconfig=[for f in fileset("${path.module}/sqlfolder", "[^_]*.yaml") : yamldecode(file("${path.module}/sqlfolder/${f}"))]
+  sqlserverlist = flatten([
+    for app in local.sqlserverconfig : [
+      for sqlserver in try(app.sqlserverconfiguration, []) :{
+        name=sqlserver.name
+      }
+    ]
+])
+}
+
+resource "azurerm_mssql_server" "azuresqlserver_yaml" {
+  for_each                     = {for value in local.sqlserverlist: "${value.name}"=>value}
+  name                         = each.value.name
+  resource_group_name          = azurerm_resource_group.francisco_sql.name
+  location                     = azurerm_resource_group.francisco_sql.location
+  version                      = var.resourceversion
+  administrator_login          = var.administrator_login
+  administrator_login_password = var.administrator_login_password
+  minimum_tls_version          = var.minimum_tls_version
+
+  azuread_administrator {
+    login_username = var.login_username
+    object_id      = var.object_id
+  }
+
+  tags = {
+    environment = var.environment
   }
 }
