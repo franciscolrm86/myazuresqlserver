5c3d4f6798f27c4766b8a414a28ee8ff98ed7aa8
Francisco Rodriguez <168039749+franciscolrm86@users.noreply.github.com>
2024-05-17 19:12:15 -0400

    Create terraform.tfvars.tf

---
+++ terraform.tfvars.tf
@@ -0,0 +1,8 @@
+name="database-rg"
+location="West Europe"
+mysqlname="mssqlserver"
+resourceversion="12.0"
+minimum_tls_version="1.2"
+login_username="AzureAD Admin"
+object_id="00000000-0000-0000-0000-000000000000"
+environment="production"
